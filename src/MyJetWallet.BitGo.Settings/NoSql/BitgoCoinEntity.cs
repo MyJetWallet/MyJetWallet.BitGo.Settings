cece8f1db861ed3c9e82483b54224bbbb896dbc1
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-05-27 16:26:39 +0300

    - use decimal to have accuracy > 15;

--- src/MyJetWallet.BitGo.Settings/NoSql/BitgoCoinEntity.cs
+++ src/MyJetWallet.BitGo.Settings/NoSql/BitgoCoinEntity.cs
@@ -32,13 +32,12 @@ namespace MyJetWallet.BitGo.Settings.NoSql
 
         public long AmountToAbsoluteValue(double amount)
         {
-            return (long) Math.Round(amount * Divider, 0);
+            return decimal.ToInt64(decimal.Round(new decimal(amount * Divider), 0));
         }
 
         public double AmountFromAbsoluteValue(long value)
         {
-            var res = value / Divider;
-            return Math.Round(res, Accuracy);
+            return decimal.ToDouble(decimal.Round(new decimal(value / Divider), Accuracy));
         }
     }
 }
